9f22c1f3b9e48723ef14feb33b5704a2bca2dcea
Limhyeonsu <dlagustn720@naver.com>
2021-02-11 12:26:54 +0900

    Thread의 우선순위

---
+++ src/ch13/ThreadEx8.java
@@ -0,0 +1,34 @@
+package ch13;
+
+public class ThreadEx8 {
+	public static void main(String[] args) {
+		//th1, th2 모두 main메서드에서 생성했기 때문에 main메서드 쓰레드의 우선순위(5)를 상속받았다.
+		ThreadEx8_1 th1 = new ThreadEx8_1();
+		ThreadEx8_2 th2 = new ThreadEx8_2();
+		//setPriority()를 통해 우선순위 변경
+		th2.setPriority(7);
+		
+		System.out.println("Priority of th1(-) : "+th1.getPriority());
+		System.out.println("Priority of th2(|) : "+th2.getPriority());
+		th1.start();
+		th2.start();
+	}
+}
+
+class ThreadEx8_1 extends Thread{
+	public void run() {
+		for (int i = 0; i < 300; i++) {
+			System.out.print("-");
+			for (int j = 0; j < 10000000; j++);
+		}
+	}
+}
+
+class ThreadEx8_2 extends Thread{
+	public void run() {
+		for (int i = 0; i < 300; i++) {
+			System.out.print("|");
+			for (int j = 0; j < 10000000; j++);
+		}
+	}
+}
